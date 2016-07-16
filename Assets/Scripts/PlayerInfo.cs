702113479bd93afdf6d2c6dbb89f83825ea3b3a5
Shin <charshin@amazon.com>
2016-07-16 12:08:27 -0700

    Added New Start Scene, script to send playername

---
+++ Assets/Scripts/PlayerInfo.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class PlayerInfo {
+	private static string playername;
+	public static string PlayerName
+	{
+		get {
+			return playername;
+		}
+		set {
+			playername = value;
+		}
+	}
+}
