65d1f13f425bef8b971fe227dcd003da44174d56
Shin <charshin@amazon.com>
2016-07-16 12:08:27 -0700

    Added New Start Scene, script to send playername

---
+++ Assets/Scripts/StartGame.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class StartGame : MonoBehaviour {
+	public UnityEngine.UI.Button button;
+	public UnityEngine.UI.Text playername;
+
+	// Use this for initialization
+	void Start () {
+		button = GameObject.Find ("StartButton").GetComponent<UnityEngine.UI.Button> ();
+		playername = GameObject.Find ("PlayerName").GetComponent<UnityEngine.UI.Text> ();
+
+		button.onClick.AddListener(() => {
+			loadGame();
+		});
+	}
+
+	void loadGame() {
+		Debug.Log (playername.text.Length);
+		if (playername.text.Length > 0) {
+			PlayerInfo.PlayerName = playername.text;
+			SceneManager.LoadScene("GameScene");
+		}
+	}
+	
+
+}
