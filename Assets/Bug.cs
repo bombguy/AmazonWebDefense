b63665a688daaedb23fc22aba2fdb4b83f406372
Baker <tylbak@amazon.com>
2016-07-16 02:27:45 -0700

    Added Amazon SDK

--- Assets/Bug.cs
+++ Assets/Bug.cs
@@ -1,16 +1,22 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+
+
 public class Bug : MonoBehaviour {
 	public 
 	// Use this for initialization
 	void Start () {
+		
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (gameObject.GetComponentInChildren<HealthBar> ().currentHealth < 0)
 			Destroy (gameObject);
+		ddbHandler ddbHandler = new ddbHandler ();
+		ddbHandler.PerformPlayStore ();
+		
 	}
 
 	void OnMouseDown() {
